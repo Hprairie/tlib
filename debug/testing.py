6637485a394b015f378a107058fb675123d96c45
hprairie <hprairie17@gmail.com>
2025-07-06 13:31:31 -0500

    small test change

--- debug/testing.py
+++ debug/testing.py
@@ -1,36 +1,8 @@
 import torch
 import triton
 import triton.language as tl
-from typing import Union, List
-
 import triton_lib as tlib
 
-
-@triton.jit
-def add(x):
-    return x + x
-
-@triton.jit
-def sub(x):
-    return x - x
-
-@tl.constexpr_function
-def function(query: str):
-    if query == "+":
-        return add
-    else:
-        return sub
-
-@triton.jit
-def rearrange(
-    query: tl.constexpr, x: tl.tensor, y: tl.tensor | None = None, z: tl.tensor | None = None) -> Union[tl.tensor, List[tl.tensor]]:
-    if y is not None:
-        return function(query)(x) * y
-    return function(query)(x)
-
-@triton.jit
-def list_work(x):
-    return [x, x]
 
 @triton.jit
 def my_kernel(
@@ -39,8 +11,6 @@ def my_kernel(
     LENGHT: tl.constexpr,
 ):
     x = tl.load(x_ptr + tl.arange(0, LENGHT)[:, None] * LENGHT + tl.arange(0, LENGHT)[None, :])
-    # x = rearrange("-", x)
-    # x, x2 = list_work(x)
     x = tlib.rearrange("a b -> b a", x)
     tl.store(o_ptr + tl.arange(0, LENGHT)[:, None] * LENGHT + tl.arange(0, LENGHT)[None, :], x)
 
